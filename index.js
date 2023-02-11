9fc97ccafe8f2467bce336ce306e076650520da1
Jorge Oliveira <kaatancontos@gmail.com>
2023-02-11 20:15:20 -0300

    11022023

---
+++ index.js
@@ -0,0 +1,5 @@
+if (!navigator.onLine) {
+  document.getElementById("msg").innerHTML = '<h2>Você não está conectado à internet</h2>';
+} else {
+  window.location.replace("https://aprendizado-jorgesouza.azurewebsites.net/");
+}
